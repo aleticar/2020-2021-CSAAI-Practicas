2dba407710bcb9994d5c7fce3991ed3ad0fcdc9e
aleticar <c.torresp.2018@alumnos.urjc.es>
2021-04-12 18:01:13 +0200

    barra abajo del canvas

--- P3/S3/canvas-01.js
+++ P3/S3/canvas-01.js
@@ -5,3 +5,22 @@ const canvas = document.getElementById("canvas");
 //-- Definir el tamaño del convas
 canvas.width = 300;
 canvas.height = 100
+
+const ctx = canvas.getContext("2d");
+
+
+
+
+
+ctx.beginPath();
+// (x,y)posicion  (largo,ancho)
+ctx.rect(0, 90, 50, 10);
+
+ctx.fillStyle="red";
+//dar color
+ctx.fill();
+//bordes
+//ctx.stroke();
+
+
+ctx.closePath();
