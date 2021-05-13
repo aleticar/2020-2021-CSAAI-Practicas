4507fc2815611f3476ff116d714daca1209f5fcd
aleticar <c.torresp.2018@alumnos.urjc.es>
2021-05-13 10:53:12 +0200

    video 2

--- P5/script.js
+++ P5/script.js
@@ -1,6 +1,7 @@
 //----- Obtener elemento de video y configurarlo
 let directo = document.getElementById("directo");
 const video1 = document.getElementById("video1");
+const video2 = document.getElementById("video2");
 const btn_video1 = document.getElementById("btn_video1");
 const btn_test = document.getElementById("btn_test");
 const btn_src_on = document.getElementById("btn_src_on");
@@ -11,7 +12,10 @@ directo.width=420;
 directo.height=200;
 video1.width=200;  
 video1.height=100;
-url="https://github.com/aleticar/videos/blob/main/BLOOPERS%20NBA_%20LeBron%20James%20en%201%20minuto%20(1).mp4";
+video2.width=200;  
+video2.height=100;
+url1="https://github.com/aleticar/videos/raw/main/BLOOPERS%20NBA%20-%20STEPHEN%20CURRY%20EN%201%20MINUTO%20(1).mp4";
+url2="https://github.com/aleticar/videos/raw/main/BLOOPERS%20NBA_%20LeBron%20James%20en%201%20minuto%20(1).mp4"
 //-- Imagen de Test usada
 const TEST_IMAGE_URL = "test.png";
 
@@ -19,6 +23,7 @@ const TEST_IMAGE_URL = "test.png";
 //-- ha arrancado
 directo.poster = TEST_IMAGE_URL;
 video1.poster = TEST_IMAGE_URL;
+video2.poster = TEST_IMAGE_URL;
 
 
 btn_src_off.onclick = () => {
@@ -28,6 +33,8 @@ btn_src_off.onclick = () => {
     directo.src = null;
     video1.poster=TEST_IMAGE_URL;
     video1.src = null;
+    video2.poster=TEST_IMAGE_URL;
+    video2.src = null;
   };
 
 
@@ -36,14 +43,20 @@ btn_src_off.onclick = () => {
 btn_src_on.onclick = () => {
  
   //-- Establecer la fuente de la cámara 1
-  video1.src=url;
+  video1.src=url1;
+  video2.src=url2;
 
   //-- Reprodeucimos un vídeo, desde el comienzo
   video1.currentTime = 0;
   video1.play();
 
+  video2.currentTime = 0;
+  video2.play();
+
+
   //-- Y en silencio...
   video1.muted=true;
+  video2.muted=true;
 
   //-- En la emisión en directo ponemos la imagen de prueba
   directo.poster = TEST_IMAGE_URL;
@@ -61,6 +74,14 @@ btn_video1.onclick = () => {
         
     directo.src = video1.src;
     directo.currentTime = video1.currentTime;
+    directo.play();
+    
+};
+btn_video2.onclick = () => {
+    
+        
+    directo.src = video2.src;
+    directo.currentTime = video2.currentTime;
     directo.play();
     
 };
