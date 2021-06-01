7982b8ce755015afe43e8f2bae668ede0ed6b4ca
aleticar <c.torresp.2018@alumnos.urjc.es>
2021-06-01 14:22:15 +0200

    FINISH

--- P2/Calc.js
+++ P2/Calc.js
@@ -40,48 +40,32 @@ const RESULT = {
   OP1: 1,
   OPERATION: 2,
   OP2: 3,
-  coma:4,
 }
 
 //-- Variable de estado
 //-- Por defecto su valor será el del estado inicial
 let estado = RESULT.INIT;
 
-for (i =0 ; i<digitos.length; i ++ ) {
-  digitos[i].onclick = (ev) => {
-    number(ev.target.value) ;
-  } 
-}
-
-for (i=0; i<operaciones.length; i++){
-  operaciones[i].onclick = (ev)=>{
-    if(estado == RESULT.OP1){
-           display.innerHTML += ev.target.value;
-           estado = RESULT.OPERATION;
-         }
-      }
-}
-
-
 function number(digito)
 {
   //-- Segun el estado hacemos una cosa u otra
   if (estado == RESULT.INIT) {
     display.innerHTML = digito;
-    estado == RESULT.OP1;}
+    estado = RESULT.OP1;}
   else if (estado == RESULT.OP1){
     display.innerHTML += digito;
   }
   else if (estado== RESULT.OPERATION) {
     display.innerHTML += digito;
-    estado == RESULT.OP2;
+    estado = RESULT.OP2;
   }
   else if (estado == RESULT.OP2){
     display.innerHTML += digito;
 
   }
-
+  
 }
+
 
 
 
@@ -96,18 +80,64 @@ for (let boton of numeros) {
   } */
 
 
+boton1.onclick = () => {
+  display.innerHTML += "1";
+}
 
+//-- Insertar digito 2
+boton2.onclick = () => {
+  display.innerHTML += "2";
+}
+boton3.onclick = () => {
+  display.innerHTML += "3";
+}
+
+boton4.onclick = () => {
+  display.innerHTML += "4";
+}
+
+//-- Insertar digitos
+boton5.onclick = () => {
+  display.innerHTML += "5";
+}
+boton6.onclick = () => {
+  display.innerHTML += "6";
+}
+
+boton9.onclick = () => {
+  display.innerHTML += "9";
+}
+
+boton0.onclick = () => {
+  display.innerHTML += "0";
+}
+
+boton7.onclick = () => {
+  display.innerHTML += "7";
+}
+boton8.onclick = () => {
+  display.innerHTML += "8";
+}
 
 //-- Insertar simbolo de suma resta...
+suma.onclick = () => {
+  display.innerHTML += "+";
+  estado = RESULT.OPERATION;
+}
 
-coma.onclick = (ev) => {
-  if(RESULT.coma=false){
-    console.log("Error!!");
-  }else{
-    display.innerHTML += ev.target.value;
-    RESULT.COMA = false;
-  }
- 
+resta.onclick = () => {
+  display.innerHTML += "-";
+}
+multi.onclick = () => {
+  display.innerHTML += "*";
+}
+
+
+div.onclick = () => {
+  display.innerHTML += "/";
+}
+coma.onclick = () => {
+  display.innerHTML += ".";
 }
 
 //-- Evaluar operacion
